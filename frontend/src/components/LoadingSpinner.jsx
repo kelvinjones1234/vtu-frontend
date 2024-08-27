61ee99de41a80e6b44c56ffa527f695a76436fc9
kelvinjones1234 <praisejournal2@gmail.com>
2024-08-27 17:48:39 +0100

    v1.0

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -4,19 +4,19 @@ const LoadingSpinner = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-bg_on">
       <div className="flex flex-col justify-center items-center">
-        <div className="relative">
+        {/* <div className="relative">
           <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-primary"></div>
           <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
             <div className="animate-ping h-5 w-5 rounded-full bg-primary"></div>
           </div>
-        </div>
-        <div className="mt-8 text-primary text-4xl font-bold">
+        </div> */}
+        <div className="mt-8 text-primary text-7xl font-bold">
           {"MaduPay".split("").map((letter, index) => (
             <span
               key={index}
               className={`inline-block animate-bounce`}
               style={{
-                animationDelay: `${index * 200}ms`,
+                animationDelay: `${index * 100}ms`,
                 animationDuration: "1s",
               }}
             >
