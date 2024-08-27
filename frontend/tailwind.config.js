ce9b53007767d443548d664c19a8b33e1ee7a2ab
kelvinjones1234 <praisejournal2@gmail.com>
2024-08-27 21:49:25 +0100

    v1.0

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -21,7 +21,7 @@ export default {
         secondary: "#00f6ff",
         dimWhite: "rgba(255, 255, 255, 0.7)",
         dimBlue: "rgba(9, 151, 124, 0.1)",
-        link: "#3B82F6"
+        link: "#0EA5E9"
       },
       fontFamily: {
         heading_one: ["Poppins", "sans-serif"],
