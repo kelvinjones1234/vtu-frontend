065afcc62d522b96605b04e7ba7799a6ea336a5f
kelvinjones1234 <praisejournal2@gmail.com>
2024-08-27 17:52:32 +0100

    v1.0

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -4,13 +4,11 @@ const LoadingSpinner = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-bg_on">
       <div className="flex flex-col justify-center items-center">
-        {/* <div className="relative">
-          <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-primary"></div>
-          <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
-            <div className="animate-ping h-5 w-5 rounded-full bg-primary"></div>
-          </div>
-        </div> */}
-        <div className="mt-8 text-primary text-7xl font-bold">
+        <div className="relative">
+          <div className="w-20 h-20 border-4 border-primary rounded-full animate-spin border-t-transparent"></div>
+          <div className="w-20 h-20 border-4 border-primary opacity-30 rounded-full absolute top-0 left-0"></div>
+        </div>
+        <div className="mt-8 text-primary text-5xl font-bold">
           {"MaduPay".split("").map((letter, index) => (
             <span
               key={index}
@@ -23,6 +21,9 @@ const LoadingSpinner = () => {
               {letter}
             </span>
           ))}
+        </div>
+        <div className="mt-4 text-primary text-lg animate-pulse">
+          Loading...
         </div>
       </div>
     </div>
