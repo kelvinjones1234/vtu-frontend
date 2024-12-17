28f8753033368b61353e31484b5e06d52f193a88
kelvinjones1234 <praisejournal2@gmail.com>
2024-12-17 03:48:01 +0100

    for production

--- frontend/src/context/GeneralContext.jsx
+++ frontend/src/context/GeneralContext.jsx
@@ -95,8 +95,8 @@ const GeneralProvider = ({ children }) => {
     }
   }, [theme]);
   const api = axios.create({
-    // baseURL: "https://madupay.pythonanywhere.com/api",
-    baseURL: "http://127.0.0.1:8000/api",
+    baseURL: "https://madupay.pythonanywhere.com/api",
+    // baseURL: "http://127.0.0.1:8000/api",
     timeout: 20000,
     headers: {
       "Content-Type": "application/json",
